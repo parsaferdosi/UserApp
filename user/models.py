787b846aafbffae081cf1e0a3e54531f11c24870
parsa <parsa78963210@gmail.com>
2025-08-23 10:43:55 +0330

    changes

--- user/models.py
+++ user/models.py
@@ -8,7 +8,7 @@ class Account(AbstractBaseUser):
     #base authentication fields
     email = models.EmailField(_("email adress"),unique=True)
     username = models.CharField(max_length=30, unique=True)
-    phone_number= models.CharField(max_length=15, blank=True, null=True)
+    phone_number= models.CharField(max_length=15,unique=True,blank=True, null=True)
     #additional fields
     PFP= models.ImageField(upload_to='profile_pics/', default='default.jpg')
     first_name = models.CharField(max_length=30, blank=True)
@@ -17,11 +17,11 @@ class Account(AbstractBaseUser):
     bio=models.TextField(blank=True, null=True)
     #status fields
     is_active = models.BooleanField(default=True)
+    # is_verified=models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-    is_verified = models.BooleanField(default=False)
     #timestamps
-    date_joined = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
     # Meta options
     USERNAME_FIELD = 'email'
