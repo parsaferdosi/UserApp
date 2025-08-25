59716e0023e3e88c444dfc419efa9142601cbabf
parsa <parsa78963210@gmail.com>
2025-08-25 21:59:12 +0330

    fix modele

--- user/models.py
+++ user/models.py
@@ -16,7 +16,6 @@ class Account(AbstractBaseUser):
     birth_date = models.DateField(null=True, blank=True)
     bio=models.TextField(blank=True, null=True)
     #status fields
-    is_active = models.BooleanField(default=True)
     # is_verified=models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
