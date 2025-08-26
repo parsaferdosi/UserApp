b564532f4b6ce0bb6f55118f60fd59bca8090962
parsa <parsa78963210@gmail.com>
2025-08-26 22:35:40 +0330

    fix

--- user/serializer.py
+++ user/serializer.py
@@ -4,8 +4,9 @@ from .models import Account
 
 # Serializer for the Account model
 class AccountSerializer(serializers.ModelSerializer):
-    model=Account
-    fields = '__all__'
-    read_only_fields = ('date_joined', 'last_updated','is_active', 'is_staff', 'is_superuser', 'is_verified')
-    required_fields = ['email', 'username', 'password']
+    class Meta:
+        model=Account
+        fields = '__all__'
+        read_only_fields = ('date_joined', 'last_updated','is_active', 'is_staff', 'is_superuser', 'is_verified')
+        required_fields = ['email', 'username', 'password']
     
